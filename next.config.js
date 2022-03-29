04acc382c52c9b6001bb578787c0098f4e8c6a29
David Chen <david.chen@alliofintech.io>
2022-03-29 15:18:56 -0700

    added basePath and export script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const json = require('./package.json');
+const folder = json.name;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: isProduction ? `/${folder}` : '',
   exportPathMap: async () => {
     return {
       '/': { page: '/' },
