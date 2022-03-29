67fd9a304dc0a1e251e6b442918a1877dd2852f3
David Chen <david.chen@alliofintech.io>
2022-03-29 11:55:06 -0700

    configured to export only desired pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  exportPathMap: async () => {
+    return {
+      '/': { page: '/' },
+    };
+  },
 };
 
 module.exports = nextConfig;
